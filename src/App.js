c88d6f220ab3c60c9e650c0692c4275d3479cdd1
Diane Leigh <leighd2008@gmail.com>
2022-12-14 08:10:49 -0500

    working react, electron app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo style={{ height: 200 }} />
+        <h1>React Posts Sharer</h1>
       </header>
     </div>
   );
